c546c5551b81e7a080e3e06bc3dcca143e93db20
Chazza <me@chazza.net>
2020-05-11 21:15:47 +0100

    Made tag unselect on join if not have permission

--- src/main/java/com/codeitforyou/tags/listener/JoinListener.java
+++ src/main/java/com/codeitforyou/tags/listener/JoinListener.java
@@ -3,15 +3,17 @@ package com.codeitforyou.tags.listener;
 import com.codeitforyou.tags.CIFYTags;
 import com.codeitforyou.tags.storage.PlayerData;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class JoinListener implements Listener {
 
-    private CIFYTags plugin;
+    private final CIFYTags plugin;
 
     public JoinListener(CIFYTags plugin) {
         this.plugin = plugin;
@@ -21,6 +23,22 @@ public class JoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(final AsyncPlayerPreLoginEvent e) {
         plugin.getStorageHandler().pullData(e.getUniqueId());
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        final Player player = e.getPlayer();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                final PlayerData playerData = PlayerData.get(player.getUniqueId());
+                if (playerData == null) return;
+                if (playerData.getTag() == null) return;
+                if (plugin.getTagManager().getTag(playerData.getTag()) == null) return;
+                if (!plugin.getTagManager().getTag(playerData.getTag()).hasPermission(player)) playerData.setTag(null);
+            }
+        }.runTaskLater(plugin, 20);
     }
 
     @EventHandler
